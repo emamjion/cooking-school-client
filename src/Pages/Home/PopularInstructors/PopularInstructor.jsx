b9fce0e4f358f5c6cd11ab12fa6ed765321f3ab2
emamjion <emamjion@gmail.com>
2023-06-12 06:03:06 +0600

    classes section and popular instruction are working

--- src/Pages/Home/PopularInstructors/PopularInstructor.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructor.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Zoom } from "react-awesome-reveal";
 
 const PopularInstructor = ({popularInstructor}) => {
     const { name, email, image } = popularInstructor;
     
     return (
-        <div className="card w-full bg-base-100 shadow-xl">
+        <Zoom>
+            <div className="card w-full bg-base-100 shadow-xl">
             <figure><img src="https://www.atptour.com/-/media/alias/player-headshot/RE44" alt="Popular Instructor" /></figure>
             <div className="card-body text-center">
                 <h2 className="card-title mx-auto">{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
+        </Zoom>
     );
 };
 
