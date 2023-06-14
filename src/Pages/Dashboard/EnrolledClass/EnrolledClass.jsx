7592c9f92bae55827069d50ce17ab197dbc996ae
emamjion <emamjion@gmail.com>
2023-06-14 23:56:46 +0600

    some code and some bug fixed

--- src/Pages/Dashboard/EnrolledClass/EnrolledClass.jsx
+++ src/Pages/Dashboard/EnrolledClass/EnrolledClass.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const EnrolledClass = () => {
-    
+    const [myEnrolled, setMyEnrolled] = useState([]);
+    useEffect(() => {
+        fetch('https://cooking-school-server.vercel.app/payments')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
     return (
         <div>
             <h1>Enrolled Class</h1>
